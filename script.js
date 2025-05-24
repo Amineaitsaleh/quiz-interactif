a0ba009845aafe1a6588b1b75cc0b70be57e3416
Aimran Alyachiri <lyaaimran@gmail.com>
2025-05-24 10:56:12 +0100

    modifier

--- script.js
+++ script.js
@@ -4,41 +4,50 @@ let questions=0;
 let data;
 let actuelReponse;
 let score=0;
-ajoutQ();//charger les questions
-$('#next-btn').click(function(){//changer les questions
-    $('#next-btn').prop('disabled',true)
-    $('.choice').prop('disabled',false)
-    actuelReponse.css('background-color','')
+ //window.location.search: rucupere le text apre ? ex:quiz.html?cat=Informatique
+    // URLSearchParams transformer ?cat=Informatique comme un objet ppour apliquee .get("cat") qui return la valeur de la cle cat 
+getdata();//charger les questions
+$('#next-btn').click(function(){//
+    $('#next-btn').prop('disabled',true)//disactiver le button next 
+    $('.choice').prop('disabled',false)//active click sur les choice
+    actuelReponse.css('background-color','')//initialiser le color de reponce choisi
     questions++;
-    ajoutQ();
+     getdata();
      })
 
-function ajoutQ(){
-     fetch("questions.json")//renvoi promise
-     .then(res=>res.json())
-     .then(res=>{
-     data=res[categore][questions]
-     $('.question').first().text(data.question)
-     for (let i=0;i<4;i++){
-        $('.choice')[i].innerHTML=data.options[i]  }
-      })
-      .catch(error=>console.error("Error lors de chargement de fichier JSON:",error));
+async function getdata(){
+    try{ 
+    let cachedQuestion=null;// //utilise pour n'est pas chaque appele lire les donnes a vec fetch
+      if(!cachedQuestion){
+    const reponse =await fetch('questions.json'); //attend la reponse de requete
+    cachedQuestion=await reponse.json(); //attend que le fichier json soit lu et converti
+      }
+      data=cachedQuestion[categore][questions]
+    $('#question').text(data.question)//ajouter le question
+     $('#choices').html('')//vider les choix précédent
+  for(i of data.options){
+   $('#choices').append(`<div class="choice">${i}</div>`)//ajouter les choix
+  }
      }
-//les evenement  de click sur choise
+   catch (error){
+  console.error(error);
+   }
+  }
+
+//les click sur les choises
 $('#choices').on('click','.choice',function(){
-    $('.choice').prop('disabled',true)
-    $('#next-btn').prop('disabled',false)
+    $('.choice').prop('disabled',true)//disactiver le click sur les choix
+    $('#next-btn').prop('disabled',false)//activer le button :pour acceder a la question suivant
     actuelReponse=$(this)
-     if(actuelReponse.text()===data.correct){
+     if(actuelReponse.text()===data.correct){//
           actuelReponse.css('background-color','green')
           score+=10;
           $('#score').text(`Score : ${score}`)
       }
      else{
        actuelReponse.css('background-color','red')
-       score=Math.max(0,score-5); 
+       score=Math.max(0,score-5); //diminue le score par 5 jusqua 0 
        $('#score').text(`Score : ${score}`)  
        }
     })
 })
-
