55e5b759b234b4501f5ec2adf428010439652d6a
unknown <amineaitsaleh22@gmail.com>
2025-05-25 00:29:41 +0100

    Corrigé les problèmes de synchronisation

--- script.js
+++ script.js
@@ -1,53 +1,45 @@
-$(document).ready(function(){
-let categore=new URLSearchParams(window.location.search).get('cat');
-let questions=0;
-let data;
-let actuelReponse;
-let score=0;
- //window.location.search: rucupere le text apre ? ex:quiz.html?cat=Informatique
-    // URLSearchParams transformer ?cat=Informatique comme un objet ppour apliquee .get("cat") qui return la valeur de la cle cat 
-getdata();//charger les questions
-$('#next-btn').click(function(){//
-    $('#next-btn').prop('disabled',true)//disactiver le button next 
-    $('.choice').prop('disabled',false)//active click sur les choice
-    actuelReponse.css('background-color','')//initialiser le color de reponce choisi
-    questions++;
-     getdata();
-     })
-
-async function getdata(){
-    try{ 
-    let cachedQuestion=null;// //utilise pour n'est pas chaque appele lire les donnes a vec fetch
-      if(!cachedQuestion){
-    const reponse =await fetch('questions.json'); //attend la reponse de requete
-    cachedQuestion=await reponse.json(); //attend que le fichier json soit lu et converti
-      }
-      data=cachedQuestion[categore][questions]
-    $('#question').text(data.question)//ajouter le question
-     $('#choices').html('')//vider les choix précédent
-  for(i of data.options){
-   $('#choices').append(`<div class="choice">${i}</div>`)//ajouter les choix
+$(document).ready(async function(){
+  let questions=[];
+  let actuelQuestion=0;
+  let score=0;
+  let categore=new URLSearchParams(window.location.search).get('cat'); // Récupère la valeur de la catégorie choisie depuis l'URL
+  
+  try {
+    const res = await fetch("questions.json"); // Charger le fichier JSON
+    const data = await res.json(); // Convertir en objet JS
+    questions = data[categore]; // Récupérer les questions de la catégorie
+  } catch (error) {
+    console.error("Erreur lors du chargement du fichier JSON:", error);
   }
-     }
-   catch (error){
-  console.error(error);
-   }
-  }
+  
+  afficheQustion();// Afficher la question
 
-//les click sur les choises
-$('#choices').on('click','.choice',function(){
-    $('.choice').prop('disabled',true)//disactiver le click sur les choix
-    $('#next-btn').prop('disabled',false)//activer le button :pour acceder a la question suivant
-    actuelReponse=$(this)
-     if(actuelReponse.text()===data.correct){//
-          actuelReponse.css('background-color','green')
+  $('#next-btn').click(function(){//passe a la question suivant
+      $('#next-btn').addClass('disabled')//disactiver le button next 
+      $('.choice').removeClass('disabled')//active click sur les choice
+      actuelQuestion++;
+      afficheQustion();
+      })
+  //les click sur les choises
+  $('#choices').on('click','.choice',function(){
+      $('.choice').addClass('disabled')//disactiver le click sur les choix
+      $('#next-btn').removeClass('disabled')//activer le button :pour acceder a la question suivant
+      if($(this).text()===questions[actuelQuestion].correct){//
+          $(this).css('background-color','#6ee7b7')
           score+=10;
-          $('#score').text(`Score : ${score}`)
+        }
+      else{
+        $(this).css('background-color','#fca5a5')
+        score=Math.max(0,score-5); //diminue le score par 5 jusqua 0   
       }
-     else{
-       actuelReponse.css('background-color','red')
-       score=Math.max(0,score-5); //diminue le score par 5 jusqua 0 
-       $('#score').text(`Score : ${score}`)  
-       }
-    })
+      $('#score').text(`Score : ${score}`)
+  })
+
+    function afficheQustion(){
+      $('#question').text(questions[actuelQuestion].question)//ajouter le question
+      $('#choices').html('')//vider les choix précédent
+    for(choix of questions[actuelQuestion].options){
+      $('#choices').append(`<div class="choice">${choix}</div>`)//ajouter les choix
+    }
+  }
 })
